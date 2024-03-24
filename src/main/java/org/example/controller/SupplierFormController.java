4222f53396241f94e34c8718fbb9004dce771d32
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-03-25 00:56:23 +0530

    designing the supplier_form.fxml

--- src/main/java/org/example/controller/SupplierFormController.java
+++ src/main/java/org/example/controller/SupplierFormController.java
@@ -1,4 +1,32 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class SupplierFormController {
+    public TextField txtSearch;
+    public TableView tblSup;
+    public TableColumn colMobile;
+    public TableColumn colFname;
+    public TableColumn colLname;
+    public TableColumn colEmail;
+    public TableColumn colCompany;
+    public TableColumn colEditbtn;
+    public TableColumn colDeletebtn;
+
+    public void searchOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void txtSearchOnAction(KeyEvent keyEvent) {
+    }
+
+    public void btnAddSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddCompanyOnAction(ActionEvent actionEvent) {
+    }
 }
