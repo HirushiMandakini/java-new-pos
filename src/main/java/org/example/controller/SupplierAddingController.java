52472cbceb09640edf671cfe7539f0b4475d69b2
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-03-25 08:23:39 +0530

    designing the supplier_adding.fxml, supplier_company_registration.fxml, supplier_update.fxml

---
+++ src/main/java/org/example/controller/SupplierAddingController.java
@@ -0,0 +1,25 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SupplierAddingController {
+    public TextField txtFname;
+    public TextField txtLname;
+    public TextField txtEmail;
+    public TextField txtAddress;
+    public TextField txtMobile;
+    public ComboBox comboCompany;
+
+    public void btnAddedSupplierOnAction(ActionEvent actionEvent) {
+    }
+    void clearFields(){
+        txtFname.clear();
+        txtLname.clear();
+        txtEmail.clear();
+        txtAddress.clear();
+        txtMobile.clear();
+        comboCompany.setValue(null);
+    }
+}
---
+++ src/main/java/org/example/controller/SupplierCompanyRegistration.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class SupplierCompanyRegistration {
+    public TextField txtCompany;
+    public TextField txtHotline;
+    public TextField txtLocation;
+    public DatePicker datePicker;
+
+    public void btnAddedCompanyOnAction(ActionEvent actionEvent) {
+    }
+}
