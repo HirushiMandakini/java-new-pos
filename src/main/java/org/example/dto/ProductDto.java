cf4bb2f24e9625d8ba7d0bcb88c60b3ed3e48a24
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-04-23 21:45:32 +0530

    add product abd invoice database tables

--- src/main/java/org/example/dto/ProductDto.java
+++ src/main/java/org/example/dto/ProductDto.java
@@ -2,13 +2,18 @@ package org.example.dto;
 
 public class ProductDto {
     private String barcode;
-    private String supName;
     private String productName;
-    private String productLocation;
     private String category;
-    private String qtyOnHand;
-    private String buyingPrice;
-    private String sellingPrice;
-    private String ManufacturerDate;
-    private String expiryDate;
+    private double costPrice;
+    private double sellingPrice;
+    private String inQty;
+    private String freeQty;
+    private String totQty;
+    private String minStockAlert;
+    private String stockBin;
+    private String invNum;
+    private String supplierName;
+    private String expDate;
+    private String mfdDate;
+
 }
